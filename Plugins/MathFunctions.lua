6f0238462ccf5c7495b88db3c62427d237baab4e
vlxveher <soniicericmartin@gmail.com>
2024-06-25 20:10:39 -0400

    Version 4

--- Plugins/MathFunctions.lua
+++ Plugins/MathFunctions.lua
@@ -11,4 +11,22 @@ function MathFunctions:RoundNumber(Number,DecimalPlaces)
 	return tonumber(string.format("%." .. (DecimalPlaces or 0) .. "f", Number))
 end
 
+function MathFunctions:SetMinimum(Num,MinNum)
+	Num = Num or 0
+	MinNum = MinNum or 0
+	if Num <= MinNum then
+		Num = MinNum
+	end
+	return Num
+end
+
+function MathFunctions:SetMaximum(Num,MaxNum)
+	Num = Num or 0
+	MaxNum = MaxNum or 0
+	if Num >= MaxNum then
+		Num = MaxNum
+	end
+	return Num
+end
+
 return MathFunctions
