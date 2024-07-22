916d3d5312531ed0b21e88add413902dbb458f2a
vlxveher <soniicericmartin@gmail.com>
2024-07-22 15:51:21 -0400

    Version 4

--- Plugins/Tools.lua
+++ Plugins/Tools.lua
@@ -5,7 +5,17 @@ Script Name : Tools
 
 ]]--
 
-local Tools = {}
+local Tools = {UI = {}}
+
+function Tools.UI:ScaleToOffset(Scale)
+	local ViewPortSize = workspace.Camera.ViewportSize
+	return ({ViewPortSize.X * Scale[1],ViewPortSize.Y * Scale[2]})
+end
+
+function Tools.UI:OffsetToScale(Offset)
+	local ViewPortSize = workspace.Camera.ViewportSize
+	return ({Offset[1] / ViewPortSize.X, Offset[2] / ViewPortSize.Y})
+end
 
 function Tools:SetModule(ModuleType,Function,Arg01,Arg02,Arg03,Arg04,Arg05)
 	coroutine.resume(coroutine.create(function()
