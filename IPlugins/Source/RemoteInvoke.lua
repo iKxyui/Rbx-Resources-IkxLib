aa1097bf2b4a21b3e27804ed0bde0b86b1adeb1f
vlxveher <soniicericmartin@gmail.com>
2025-02-10 06:05:48 -0500

    init

--- IPlugins/Source/RemoteInvoke.lua
+++ IPlugins/Source/RemoteInvoke.lua
@@ -7,17 +7,16 @@ Script Name : RemoteInvoke
 
 local RemoteInvoke = {}
 
-function RemoteInvoke:InvokeAllClients(RemoteInstance,Var1,Var2,Var3,Var4,Var5,Var6,Var7,Var8,Var9,Var10)
-	coroutine.resume(coroutine.create(function()
+function RemoteInvoke:InvokeAllClients(RemoteInstance,Arg01,Arg02,Arg03,Arg04,Arg05,Arg06,Arg07,Arg08,Arg09)
+	task.spawn(function()
 		for _,GetPlayerObj in pairs(game:GetService("Players"):GetPlayers()) do
-			coroutine.resume(coroutine.create(function()
+			task.spawn(function()
 				pcall(function()
-					return RemoteInstance:InvokeClient(GetPlayerObj,Var1,Var2,Var3,Var4,Var5,Var6,Var7,Var8,Var9,Var10)
+					return RemoteInstance:InvokeClient(GetPlayerObj,Arg01,Arg02,Arg03,Arg04,Arg05,Arg06,Arg07,Arg08,Arg09)
 				end)
-				return GetPlayerObj
-			end))
+			end)
 		end
-	end))
+	end)
 end
 
 return RemoteInvoke
